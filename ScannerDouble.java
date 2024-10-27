02fc33d568daae415c31d71a16f63e1b5c23f910
Osman OK <161768567+mevocell@users.noreply.github.com>
2024-10-27 17:51:11 +0300

    Add files via upload

---
+++ ScannerDouble.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ScannerDouble {
+    public static void main(String[] args) {
+        getScannerDouble();
+    }
+
+    public static void getScannerDouble() {
+        Scanner getScanner= new Scanner(System.in);
+        
+    }
+}
