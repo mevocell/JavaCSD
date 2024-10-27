888d96ad87cd7ab02403bdb71c61adadd2767005
Osman OK <161768567+mevocell@users.noreply.github.com>
2024-10-27 18:54:17 +0300

    ScannerDouble.java

--- ScannerDouble.java
+++ ScannerDouble.java
@@ -7,6 +7,19 @@ public class ScannerDouble {
 
     public static void getScannerDouble() {
         Scanner getScanner= new Scanner(System.in);
-        
+        System.out.println("Birinci Sayıyı Giriniz.");
+        double numberOne=getScanner.nextDouble();
+        System.out.println("İkinci Sayıyı Giriniz.");
+        double numberTwo=getScanner.nextDouble();
+        System.out.println("Girilen Birinci Sayı: "+numberOne);
+        System.out.println("İkinci Girilen Sayı :"+numberTwo);
+        double sum=numberOne+numberTwo;
+        System.out.println("Girilen Sayıların Toplamı :"+sum);
+        double subtraction =numberOne-numberTwo;
+        System.out.println("Girilen Sayıların Farkı :"+subtraction);
+        double multiplication = numberOne*numberTwo;
+        System.out.println("Girilen Sayıların Çarpımı :"+multiplication);
+        double division=numberOne/numberTwo;
+        System.out.println("Girilen Sayıların Bölümü :"+division);
     }
 }
